dda8ef9224b54db8d5fe3544eef41df5cf614d51
tgv293 <102977775+tgv293@users.noreply.github.com>
2023-10-04 21:49:45 +0700

    update

--- BMICalculator/app/src/main/java/com/giapvantai/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/giapvantai/bmicalculator/MainActivity.java
@@ -3,12 +3,54 @@ package com.giapvantai.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Khai báo các thành phần giao diện
+    EditText edtHeight, edtWeight;
+    TextView tvResult, tvRStatus;
+    Button btnCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Tìm các điều khiển
+        edtHeight = findViewById(R.id.edtHeight);
+        edtWeight = findViewById(R.id.edtWeight);
+        tvResult = findViewById(R.id.tvResult);
+        tvRStatus = findViewById(R.id.tvRStatus);
+        btnCalculate = findViewById(R.id.btnCalculate);
+
+        // Thêm lắng nghe sự kiện cho nút btnCalculate
+        btnCalculate.setOnClickListener(view -> calculateBMI());
+    }
+
+    private void calculateBMI() {
+        float height = Float.parseFloat(edtHeight.getText().toString()) / 100;
+        float weight = Float.parseFloat(edtWeight.getText().toString());
+        float bmi = weight / (height * height);
+
+        tvResult.setText(String.format("%.2f", bmi));
+        tvRStatus.setText(getBMICategory(bmi));
+    }
+
+    private String getBMICategory(float bmi) {
+        if (bmi < 18.5) {
+            return "Cân nặng thấp (gầy)";
+        } else if (bmi < 25) {
+            return "Bình thường";
+        } else if (bmi == 25) {
+            return "Thừa cân";
+        } else if (bmi < 30) {
+            return "Tiền béo phì";
+        } else if (bmi < 35) {
+            return "Béo phì độ I";
+        } else {
+            return "Béo phì độ II";
+        }
     }
 }
